4d781892a65d181f9a78428e6cebbfc5f872e522
Joeri Smits <joeri.smits@planet.nl>
2014-10-16 14:09:58 +0200

    Modified app.js

--- Implementatie/app.js
+++ Implementatie/app.js
@@ -3,5 +3,13 @@
  */
 
 var express = require('express');
+var path = require('path');
 
 var app = express();
+
+// Using directory client-side as client directory.
+app.use(express.static(path.join(__dirname, 'client-side')));
+
+// Running the app
+app.listen(3000);
+console.log("App running on localhost:3000");
