1589f4e09c5080d16db49f333a4de059f8334121
Joeri Smits <joeri.smits@planet.nl>
2014-10-16 15:02:32 +0200

    Added team model

--- Implementatie/app.js
+++ Implementatie/app.js
@@ -4,11 +4,16 @@
 
 var express = require('express');
 var path = require('path');
+var mongoose = require('mongoose');
 
 var app = express();
 
 // Using directory client-side as client directory.
 app.use(express.static(path.join(__dirname, 'client-side')));
+
+mongoose.connect('mongodb://localhost:27017/kwizzert');
+
+var Team = require(path.join(__dirname, './models/team.js'));
 
 // Running the app
 app.listen(3000);
---
+++ Implementatie/models/team.js
@@ -0,0 +1,8 @@
+var mongoose = require('mongoose');
+
+var teamSchema = new mongoose.Schema({
+    name: String,
+    score: Number
+});
+
+module.exports = mongoose.model('Team', teamSchema);
